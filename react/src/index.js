ed8dadaa58a6af495f185a2f02760d5d91fc52c6
RomanBoegli <22320200+RomanBoegli@users.noreply.github.com>
2026-06-25 15:04:50 +0200

    switch primeflex to css

--- react/src/index.js
+++ react/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
